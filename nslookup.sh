ad072fb51c9e353b7a08802e7203a747727429f9
atomic-sprat <melankhar@gmail.com>
2025-03-27 23:25:17 +0300

    Update nslookup.sh
    fix legacy env and comments

--- nslookup.sh
+++ nslookup.sh
@@ -6,21 +6,20 @@ if [ ! -f "hosts.txt" ]; then
   exit 1
 fi
 
-# Очищаем файл ip.txt, если он существует
-> ip.txt
+# Очищаем файл routes.txt, если он существует
+> routes.txt
 
 # Читаем файл hosts.txt построчно
 while IFS= read -r host; do
   # Выполняем nslookup для текущего хоста и получаем только IP-адреса
   ips=$(nslookup "$host" | awk '/^Address: / { print $2 }')
 
-  # Для каждого найденного IP-адреса проверяем, что это IPv4, и записываем в ip.txt
+  # Для каждого найденного IP-адреса проверяем, что это IPv4
   for ip in $ips; do
-    # Проверяем, что это IPv4-адрес (он должен состоять из 4 чисел, разделенных точками)
     if [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-      echo "$ip/32" >> ip.txt
+      echo "$ip/32" >> routes.txt
     fi
   done
 done < hosts.txt
 
-echo "IPv4-адреса сохранены в ip.txt"
+echo "Маршруты сохранены в routes.txt"
